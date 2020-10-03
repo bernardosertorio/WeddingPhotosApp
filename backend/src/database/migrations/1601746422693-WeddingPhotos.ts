5448c181d419bb1db69ec5aa17b9d30eff2242cf
Bernardo Sertório <besertorio@gmail.com>
2020-10-03 16:18:45 -0300

    create migration/database

---
+++ backend/src/database/migrations/1601746422693-WeddingPhotos.ts
@@ -0,0 +1,30 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export default class WeddingPhotos1601746422693
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'wedding',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'weddingPhotos',
+            type: 'varchar',
+            isNullable: false,
+          }
+        ]
+      }),
+    );
+    }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('wedding')
+    }
+
+}
