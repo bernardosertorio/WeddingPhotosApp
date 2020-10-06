bb3706f849ce56900bb2b42bb11ccabd00b9c0f4
Bernardo Sertório <besertorio@gmail.com>
2020-10-06 20:18:30 -0300

    set SavingWeddingPhotosService

---
+++ backend/src/services/SavingWeddingPhotosService.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm';
+import WeddingPhotos from '../models/WeddingPhotos';
+import AppError from '../errors/AppError';
+
+interface Request {
+  filename: string;
+}
+
+class SavingWeddingPhotosService {
+  public async execute({ filename }: Request): Promise<WeddingPhotos> {
+    const weddingPhotosRepository = getRepository(WeddingPhotos);
+
+    const checkPhotoExists = await weddingPhotosRepository.findOne({
+      where: { filename },
+    });
+
+    if (checkPhotoExists) {
+      throw new AppError('This photo already exists', 401);
+    }
+
+    const newWeddingPhotos = weddingPhotosRepository.create({
+      weddingPhotos: filename,
+    });
+
+    await weddingPhotosRepository.save(newWeddingPhotos);
+
+    return newWeddingPhotos;
+  }
+}
+
+export default SavingWeddingPhotosService;
