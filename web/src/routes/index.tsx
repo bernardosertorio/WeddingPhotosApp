57d89803fa0a087f9f2ffdf6adc82a6a9bc35715
Bernardo Sertório <besertorio@gmail.com>
2020-10-07 16:12:45 -0300

    set upload and Gallery pages

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Galery from '../pages/Galery';
 
 import UploadPhotos from '../pages/UploadPhotos';
-import Galery from '../pages/Galery';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={UploadPhotos} />
-    <Route path="/" component={Galery} />
+    <Route path="/galery" component={Galery} />
   </Switch>
 );
 
